b36f96749a1b4f6cb36bc0e98e32cbfe3723b7a3
John Garcia <john.garcia@ajila.com>
2021-04-08 22:45:07 +0200

    sapper/POC: deploy issue

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Nav from '../components/Nav.svelte';
-	import Footer from '../components/Footer.svelte';
+/* 	import Footer from '../components/Footer.svelte'; */
 
 	export let segment;
 </script>
@@ -22,4 +22,4 @@
 <main>
 	<slot></slot>
 </main>
-<Footer/>
+<!-- <Footer/> -->
