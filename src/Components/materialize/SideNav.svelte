917b32edfd90ad28122fd7008b53f0773c51a049
John Garcia <john.garcia@ajila.com>
2021-04-08 11:36:49 +0200

    gallery component refactored, gallery items added via module, gallery-item created, components init module working

--- src/Components/materialize/SideNav.svelte
+++ src/Components/materialize/SideNav.svelte
@@ -1,9 +1,9 @@
-<ul class="side-nav" id="nav-mobile">
-    <li class="k"><a href="/pages/demo"><i class="material-icons">camera</i>Gallery</a></li>
-    <li><a href="/pages/dark-theme"><i class="material-icons">brightness_3</i>Dark Theme</a></li>
-    <li><a href="/pages/blog"><i class="material-icons">edit</i>Blog</a></li>
-    <li><a href="/pages/docs"><i class="material-icons">school</i>Docs</a></li>
-    <li><a href="/pages/full-header"><i class="material-icons">fullscreen</i>Fullscreen Header</a></li>
-    <li><a href="/pages/horizontal"><i class="material-icons">swap_horiz</i>Horizontal Style</a></li>
-    <li><a href="/pages/no-image"><i class="material-icons">texture</i>No Image Expand</a></li>
-  </ul>
+<ul class="sidenav" id="nav-mobile">
+  <li class="active"><a href="index.html"><img src="Assets/images/logo-2.svg" with="40" height="40" alt="gwendolin logo"></a></li>
+  <li><a href="index-dark.html"><i class="material-icons">brightness_3</i>Dark Theme</a></li>
+  <li><a href="blog.html"><i class="material-icons">edit</i>Blog</a></li>
+  <li><a href="docs.html"><i class="material-icons">school</i>Docs</a></li>
+  <li><a href="full-header.html"><i class="material-icons">fullscreen</i>Fullscreen Header</a></li>
+  <li><a href="horizontal.html"><i class="material-icons">swap_horiz</i>Horizontal Style</a></li>
+  <li><a href="no-image.html"><i class="material-icons">texture</i>No Image Expand</a></li>
+</ul>
